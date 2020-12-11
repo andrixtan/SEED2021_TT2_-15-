498a3f2d3cd230e6f7a327b52e56f3a37767e864
Javiertamama <75264884+Javiertamama@users.noreply.github.com>
2020-12-11 14:26:52 +0800

    reload to acc

--- Group15/login.js
+++ Group15/login.js
@@ -4,28 +4,29 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-    Calling()
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+  Calling();
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
-
-
+  if (username === "user" && password === "web_dev") {
+    alert("You have successfully logged in.");
+    location.href = "accpage.html";
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+});
 
 function Calling() {
-	fetch('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view')
-	  	.then(response => {
-	  		return response.json()})
-	  	.then(data => {
-	  		console.log(data)
-	  	})
+  fetch(
+    "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
 
 // document.getElementById("login-form-submit").onclick = Calling
