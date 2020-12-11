1f2a3f1cb655b5483aeb9c73a14c4fad3f694ce6
Javiertamama <75264884+Javiertamama@users.noreply.github.com>
2020-12-11 16:06:13 +0800

    updates on css

--- Group15/login.js
+++ Group15/login.js
@@ -4,18 +4,19 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-    Calling()
-
-    // if (username === "user" && password === "web_dev") {
-    //     alert("You have successfully logged in.");
-    //     location.reload();
-    // } else {
-    //     loginErrorMsg.style.opacity = 1;
-    // }
-})
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+  Calling();
+  /*
+  if (username === "user" && password === "web_dev") {
+    alert("You have successfully logged in.");
+    window.location = "accpage.html";
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+  */
+});
 
 // var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
 //     targetUrl = 'http://catfacts-api.appspot.com/api/facts?number=99'
@@ -32,27 +33,31 @@ loginButton.addEventListener("click", (e) => {
 //   });
 
 function Calling() {
-    var myHeaders = new Headers();
-    myHeaders.append("x-api-key", "UvNtOZVEB61nTaqAj9x9640XEK81qnHN7fSrEOMF");
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({username:"Group15",password:"WoAzAGZv_RGnusp"});
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = 'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login'
+  var myHeaders = new Headers();
+  myHeaders.append("x-api-key", "UvNtOZVEB61nTaqAj9x9640XEK81qnHN7fSrEOMF");
+  myHeaders.append("Content-Type", "application/json");
 
-   fetch(proxyUrl + targetUrl, requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+  var raw = JSON.stringify({
+    username: "Group15",
+    password: "WoAzAGZv_RGnusp",
+  });
 
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  var proxyUrl = "https://cors-anywhere.herokuapp.com/",
+    targetUrl =
+      "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login";
+
+  fetch(proxyUrl + targetUrl, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
+
 // function Calling() {
 // 	fetch('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view')
 // 	  	.then(response => {response.json()})
