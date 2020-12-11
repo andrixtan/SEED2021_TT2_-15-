ccd616787e38277b64f2ab386a2e83214546754f
Andrix Tan <andrixtan_96@hotmail.com>
2020-12-11 14:08:27 +0800

    edited abit of the login page

--- Group15/login.js
+++ Group15/login.js
@@ -1,9 +1,23 @@
 "use strict";
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-// async Calling() {
-// 	const response = await fetch('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login');
-// 	var data = await response.json();
-// }
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    Calling()
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+
 
 function Calling() {
 	fetch('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view')
@@ -14,4 +28,4 @@ function Calling() {
 	  	})
 }
 
-document.getElementById("button").onclick = Calling
+// document.getElementById("login-form-submit").onclick = Calling
